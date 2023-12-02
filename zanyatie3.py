a01bd7f5a9690b24ea9ad559963a2aa2f792d437
bobr234 <152699234+bobr234@users.noreply.github.com>
2023-12-02 15:46:34 +0300

    newbranch

---
+++ zanyatie3.py
@@ -0,0 +1,35 @@
+marks = [3,4,2,5,5.5.3,4,2,5,3]
+print(marks[1])
+print(marks[-1])
+for i in range()
+
+a = [1, 3, 4, 5, -5]
+res = 0
+ 
+for x in range(0, len(a)-1):
+    if type(a[x]) == type(int()):
+        res += a[x]
+ 
+print("res"
+      
+      
+      
+      
+      
+
+      A = []
+    max = ''
+    min = ''
+    a = int(input('Введите целое число...'))
+    c = int(input('Введите целое число...'))
+    d = int(input('Введите целое число...'))
+    e = int(input('Введите целое число...'))
+    f = int(input('Введите целое число...'))
+    A += a,c,d,e,f
+    for i in A:
+        if i == max:
+            max += i
+        else:
+            min += i
+    
+    print(f'Максимальное: {max}' , f'Минимальное: {min}' )
